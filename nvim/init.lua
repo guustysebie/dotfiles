0982af7e058214efb80c28b99501f7f2ceec1a26
Guust Ysebie <40126344+guustysebie@users.noreply.github.com>
2024-12-19 12:51:14 +0100

    Update init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -16,7 +16,7 @@ vim.opt.colorcolumn = "120"             -- Set an 80 column border
 --vim.opt.cursorline = true              -- Highlight current line
 vim.opt.ttyfast = true                 -- Speed up scrolling
 vim.opt.clipboard = "unnamedplus"      -- Use system clipboard
-vim.wo.wrap = false                    -- Disable long lines wrapping around
+
 -- Syntax Highlighting and Filetype Detection
 vim.cmd("filetype plugin indent on")   -- Enable file type detection and plugins
 vim.cmd("syntax on")                   -- Enable syntax highlighting
@@ -53,3 +53,53 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 vim.cmd [[colorscheme habamax]]
 
+-- REMAPS --
+vim.g.mapleader = " "
+-- open netrw 
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+
+
+
+
+
+--  Plugins ----
+--
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+    if vim.v.shell_error ~= 0 then
+        vim.api.nvim_echo({
+            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+            { out, "WarningMsg" },
+            { "\nPress any key to exit..." },
+        }, true, {})
+        vim.fn.getchar()
+        os.exit(1)
+    end
+end
+vim.opt.rtp:prepend(lazypath)
+
+
+
+require("lazy").setup({
+    spec = {
+        -- add your plugins here
+        {
+            'nvim-telescope/telescope.nvim', tag = '0.1.8'
+        }
+    },
+    -- Configure any other settings here. See the documentation for more details.
+    -- colorscheme that will be used when installing plugins.
+    install = { colorscheme = { "habamax" } },
+    -- automatically check for plugin updates
+    checker = { enabled = true },
+})
+-- Telescope config
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
